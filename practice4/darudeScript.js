647d45f8f6a76786f2a8608c9580b98a8f372b2b
Teemu Ritonen <teemu.ritonen@adafy.com>
2022-05-19 16:08:17 +0300

    practice 4 done

--- practice4/darudeScript.js
+++ practice4/darudeScript.js
@@ -1,8 +1,25 @@
 
 
-$(document).ready(function () {
-    $.getJSON("https://itunes.apple.com/lookup?id=1472380750&entity=album", function (data) {
-        $("h2").text("bugger");
-    }) 
-        
-});
+$(document).ready(function getRecords() {
+    $.ajax({
+        url: "https://itunes.apple.com/search?term=darude",
+
+        jsonp: "callback",
+
+        dataType: "jsonp",
+
+        data: {
+            q: "select title,abstract,url from search.news where query=\"cat\"",
+            format: "json"
+        },
+
+        success: function (response) {
+            $.each(response.results, function (i, results) {
+                $("#cardRow").append("<div class='card m-2' style='width: 200px; height: 300px'><img src = ' " + response.results[i].artworkUrl100 + "' class='card - image - top' alt = 'darude album cover' /><div class='card - body'><h5 class='card - title mt-3'>" + response.results[i].collectionName + "</h5></div></div >");
+                if (i === 50) {
+                    return false;
+                }
+            });
+        }
+    });
+})
